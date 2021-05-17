8ebd64ae6ffebffc9c4e7bb9cc588d95812664af
Gennady <vasilievga@yandex.ru>
2021-05-17 15:19:22 +0300

    For review 17-05-2021

--- homework.py
+++ homework.py
@@ -11,29 +11,21 @@ load_dotenv()
 
 
 PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
-AUTHORIZATION_HEADER = f'OAuth {PRAKTIKUM_TOKEN}'
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}/'}
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 HOMEWORK_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    filename='ya_hw_bot.log',
-    filemode='a',
-    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
-)
-
-# настройка логгера 1
-log_path = os.path.expanduser('~/./ya_hw_bot.log')
+# настройка логгера
+log_path = os.path.expanduser('~/ya_hw_bot.log')
 logger = logging.getLogger(__name__)
-logging.getLogger('urlib3').setLevel(logging.WARNING)
-logging.getLogger('telegram').setLevel(logging.WARNING)
 handler = RotatingFileHandler(log_path, maxBytes=2000000, backupCount=5)
 handler.setLevel(logging.INFO)
 logger.addHandler(handler)
 
-HOMEWORK_STATUS = {
+START_MESSAGE = 'Запуск программы'
+STATUS_MESSAGE_DICT = {
     'rejected': 'У вас проверили работу "{homework}"!\n\n'
                 'К сожалению в работе нашлись ошибки.',
     'reviewing': 'Работа {homework} взята в ревью',
@@ -41,52 +33,44 @@ HOMEWORK_STATUS = {
                 'Ревьюеру всё понравилось, можно '
                 'приступать к следующему уроку.',
 }
-HOMEWORK_STATUS2 = {
-    'rejected': 'К сожалению в работе нашлись ошибки.',
-    'reviewing': 'Работа {homework} взята в ревью',
-    'approved': 'Ревьюеру всё понравилось, можно '
-                'приступать к следующему уроку.',
-}
-ERROR_HW_STATUS = 'Ошибка статуса при запросе {homework}'
-ERROR_MESSAGE = 'При запросе данных бот столкнулся с ошибкой:'
-SERVER_ERROR_MESSAGE = 'Ошибка ответа сервера'
-START_MESSAGE = 'Запуск программы'
-KEY_NOT_FOUND_MESSAGE = 'Ключ не найден'
+ERROR_STATUS = 'При запросе {homework} ошибка статуса: {status}'
+ERROR_MESSAGE = ('При запросе данных бот столкнулся с ошибкой: '
+                 '{error}.'
+                 '\n.Параметры запроса:')
+SERVER_REFUSAL_MESSAGE = ('Отказ от выполнения работы сервера. '
+                          'Вернулся ключ "{key}".'
+                          '\nПараметры запроса:')
 MESSAGE_SENT = 'Сообщение отправлено'
+ERROR_SENT_TO_BOT_MESSAGE = ('При отправке сообшения в Телеграм бот '
+                             'столкнулся с ошибкой {error}')
 
 
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
-    if homework['status'] not in HOMEWORK_STATUS.keys():
-        raise ValueError(ERROR_HW_STATUS.format(homework=homework_name))
-    for key, value in HOMEWORK_STATUS.items():
-        if homework['status'] == key:
-            status = value.format(homework=homework_name)
-    return status
+    status = homework['status']
+    if status not in STATUS_MESSAGE_DICT:
+        raise ValueError(
+            f'{ERROR_STATUS.format(status=status)}')
+    return STATUS_MESSAGE_DICT[status].format(homework=homework_name)
 
 
 def get_homework_statuses(current_timestamp):
-    json_error_codes = ['code', 'error']
-    headers = {'Authorization': AUTHORIZATION_HEADER}
     # params = {'from_date': 0}  # параметр для отладки
     params = {'from_date': current_timestamp}
+    request_data = dict(url=HOMEWORK_URL, headers=HEADERS, params=params)
     try:
-        response = requests.get(
-            url=HOMEWORK_URL,
-            headers=headers,
-            params=params
-        )
-    except Exception as error:
-        exception_text = (f'{ERROR_MESSAGE} {error}')
-        logger.error(f'{exception_text}', exc_info=True)
-        bot_client.send_message(CHAT_ID, exception_text)
-    else:
+        response = requests.get(**request_data)
         data = response.json()
-        for key in json_error_codes:
+        for key in ['code', 'error']:
             if key in data:
-                raise ValueError(SERVER_ERROR_MESSAGE)
-                logger.error(f'{exception_text}', exc_info=True)
+                raise ValueError(
+                    f'{SERVER_REFUSAL_MESSAGE.format(key=key)}'
+                    f'{request_data}')
         return data
+    except requests.exceptions.RequestException as error:
+        logger.error(
+            f'{ERROR_MESSAGE.format(error=error)}'
+            f'{request_data}')
 
 
 def send_message(message, bot_client):
@@ -94,6 +78,16 @@ def send_message(message, bot_client):
 
 
 def main():
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='ya_hw_bot.log',
+        filemode='a',
+        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
+    )
+    logging.getLogger('urlib3').setLevel(logging.WARNING)
+    logging.getLogger('telegram').setLevel(logging.WARNING)
+
     logger.debug(START_MESSAGE)
     current_timestamp = int(time.time())
 
@@ -105,17 +99,12 @@ def main():
                     new_homework.get("homeworks")[0]), bot_client)
             current_timestamp = new_homework.get(
                 'current_date', current_timestamp)
-            time.sleep(300)
-            raise KeyError(KEY_NOT_FOUND_MESSAGE)
-        except Exception as error:
-            logger.error(f'{error}', exc_info=True)
-            try:
-                bot_client.send_message(CHAT_ID, {error})
-            except Exception as error:
-                logger.debug(f'{error}', exc_info=True)
-            time.sleep(300)
-        else:
             logger.info(MESSAGE_SENT)
+            time.sleep(300)
+        except Exception as error:
+            logger.error(ERROR_SENT_TO_BOT_MESSAGE.format(
+                         error={error}), exc_info=True)
+            time.sleep(300)
 
 
 if __name__ == '__main__':
