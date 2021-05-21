4c87162ddcf8b4594a629df488fc0df75fa94a2c
Gennady <vasilievga@yandex.ru>
2021-05-21 14:39:39 +0300

    For review 21.05.2021

--- homework.py
+++ homework.py
@@ -2,7 +2,6 @@ import logging
 import os
 import time
 from logging.handlers import RotatingFileHandler
-from urllib.request import HTTPError
 
 import requests
 import telegram
@@ -26,7 +25,7 @@ handler.setLevel(logging.INFO)
 logger.addHandler(handler)
 
 START_MESSAGE = 'Запуск программы'
-VERDICT_MESSAGE_DICT = {
+VERDICTS_DICT = {
     'rejected': 'У вас проверили работу "{homework}"!\n\n'
                 'К сожалению в работе нашлись ошибки.',
     'reviewing': 'Работа {homework} взята в ревью',
@@ -35,47 +34,48 @@ VERDICT_MESSAGE_DICT = {
                 'приступать к следующему уроку.',
 }
 UNEXPECTED_STATUS = (
-    'При запросе {homework} бот столкнулся '
+    'При запросе "{homework}" бот столкнулся '
     'с неожиданным статусом: {status}'
 )
 ERROR_MESSAGE = (
-    'При запросе данных бот столкнулся с ошибкой: '
-    '{error}.\n.Параметры запроса: {data}'
+    'При запросе данных бот столкнулся с ошибкой: "{error}".'
+    '\nПараметры запроса: {data}'
 )
 SERVER_REFUSAL_MESSAGE = (
-    'Отказ от выполнения работы сервера. .'
-    'Причина отказа сервера: {reason}'
+    'Отказ от выполнения работы сервера. '
+    'Причина отказа сервера: "{reason}"'
     '\nПараметры запроса: {data}'
 )
 MESSAGE_SENT = 'Отправлено следующее сообщение: \n <{message}>\n'
-BOT_ERROR_MESSAGE = ('Бот столкнулся с ошибкой {error}')
+BOT_ERROR_MESSAGE = ('Бот столкнулся с ошибкой: {error} \n'
+                     'От сервера получен следующий ответ: {data}')
 
 
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
     status = homework['status']
-    if status not in VERDICT_MESSAGE_DICT:
+    if status not in VERDICTS_DICT:
         raise ValueError(
-            UNEXPECTED_STATUS.format(status=status, homework=homework_name))
-    return VERDICT_MESSAGE_DICT[status].format(homework=homework_name)
+            UNEXPECTED_STATUS.format(
+                status=status, homework=homework_name))
+    return VERDICTS_DICT[status].format(homework=homework_name)
 
 
 def get_homework_statuses(current_timestamp):
-    # params = {'from_date': 0}  для отладки
-    # params = {'from_date': current_timestamp} для проекта
     request_data = dict(url=HOMEWORK_URL, headers=HEADERS,
-                        params={'from_date': 0})
+                        params={'from_date': current_timestamp})
     try:
         response = requests.get(**request_data)
     except requests.exceptions.RequestException as error:
-        raise HTTPError(
-            ERROR_MESSAGE.format(error=error, **request_data))
+        raise ConnectionError(
+            ERROR_MESSAGE.format(error=error, data=request_data)
+        )
     data = response.json()
     for key in ['code', 'error']:
         if key in data:
             raise RuntimeError(
-                SERVER_REFUSAL_MESSAGE.format(reason=data[key],
-                                              **request_data))
+                SERVER_REFUSAL_MESSAGE.format(
+                    reason=data[key], data=request_data))
     return data
 
 
@@ -88,19 +88,20 @@ def main():
     current_timestamp = int(time.time())
 
     while True:
+        new_homework = get_homework_statuses(current_timestamp)
         try:
-            new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get("homeworks"):
                 message = parse_homework_status(
                     new_homework.get("homeworks")[0])
                 send_message(message, bot_client)
+                logger.info(MESSAGE_SENT.format(message=message))
             current_timestamp = new_homework.get(
                 'current_date', current_timestamp)
-            logger.info(MESSAGE_SENT.format(message=message))
+
             time.sleep(300)
         except Exception as error:
             logger.error(BOT_ERROR_MESSAGE.format(
-                         error={error}), exc_info=True)
+                         error=error, data=new_homework), exc_info=True)
             time.sleep(300)
 
 
@@ -115,3 +116,57 @@ if __name__ == '__main__':
     logging.getLogger('telegram').setLevel(logging.WARNING)
 
     main()
+
+    # # сбой сети
+    # from unittest import TestCase, mock, main as uni_main # noqa
+    # ReqEx = requests.RequestException
+
+    # class TestReq(TestCase):
+    #     @mock.patch('requests.get')
+    #     def test_raised(self, rq_get):
+    #         rq_get.side_effect = mock.Mock(
+    #             side_effect=ReqEx('testing'))
+    #         main()
+    # uni_main()
+
+    # # отказ сервера
+    # from unittest import TestCase, mock, main as uni_main
+    # JSON = {'error': 'testing'}
+
+    # class TestReq(TestCase):
+    #     @mock.patch('requests.get')
+    #     def test_error(self, rq_get):
+    #         resp = mock.Mock()
+    #         resp.json = mock.Mock(
+    #             return_value=JSON)
+    #         rq_get.return_value = resp
+    #         main()
+    # uni_main()
+
+    # # неожиданный статус
+    # from unittest import TestCase, mock, main as uni_main
+    # JSON = {'homeworks': [{'homework_name': 'test', 'status': 'test'}]}
+
+    # class TestReq(TestCase):
+    #     @mock.patch('requests.get')
+    #     def test_error(self, rq_get):
+    #         resp = mock.Mock()
+    #         resp.json = mock.Mock(
+    #             return_value=JSON)
+    #         rq_get.return_value = resp
+    #         main()
+    # uni_main()
+
+    # # некорректный json
+    # from unittest import TestCase, mock, main as uni_main
+    # JSON = {'homeworks': 1}
+
+    # class TestReq(TestCase):
+    #     @mock.patch('requests.get')
+    #     def test_error(self, rq_get):
+    #         resp = mock.Mock()
+    #         resp.json = mock.Mock(
+    #             return_value=JSON)
+    #         rq_get.return_value = resp
+    #         main()
+    # uni_main()
